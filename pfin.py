4aca88ec4627fb04c58c8819ff6a509a675a8ef0
therealkuma <108306648+therealkuma@users.noreply.github.com>
2023-08-25 15:37:34 -0400

    Update pfin.py

--- pfin.py
+++ pfin.py
@@ -206,7 +206,7 @@ if authentication_status:
                     st.plotly_chart(fig_bar)
                     
                     #### monthly total expense bar chart  ###
-                    total_monthly = categorized_df.groupby([categorized_df['Date'].dt.to_period('M'))
+                    total_monthly = categorized_df.groupby(categorized_df['Date'].dt.to_period('M'))
                     total_monthly= total_monthly[~total_monthly['Description'].str.contains("AUTOPAY")]['Amount'].sum()
                     # Draw bar chart for month-to-month comparison
                     total_fig_bar = px.bar(x=total_monthly.index.astype(str), y=total_monthly.values)
